25e9522e66bf338fa2d99b3f959d9d8b6de9dcb8
Petr Čaněk <canekpet@gmail.com>
2019-08-15 10:14:23 +0200

    added base of listing and header

---
+++ src/components/listing/Listing.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+ * What do need
+ * Data
+ * output them in a list
+ * add ability to go to detail
+ */
+
+function Listing() {
+  return (
+    <div>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Listing;
