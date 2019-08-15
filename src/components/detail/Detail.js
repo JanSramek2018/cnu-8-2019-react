c17a4e785db45bbd2f44a10904ede4b2640a0037
Petr Čaněk <canekpet@gmail.com>
2019-08-15 13:29:04 +0200

    added data loading to detail

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,14 +1,41 @@
 import React from 'react';
 
-function Detail(props) {
-  const { goToListing, detailId } = props;
-  console.log('detailId', detailId);
-  return (
-    <div>
-      <h2>This is detail</h2>
-      <button onClick={goToListing}>Go to Listing</button>
-    </div>
-  );
+const API_URL = 'https://cookbook.jakubricar.cz/api/recipes/';
+
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: false,
+    };
+  }
+
+  componentDidMount = () => {
+    const { detailId } = this.props;
+
+    const detailUrl = `${API_URL}${detailId}`;
+
+    fetch(detailUrl)
+      .then(response => response.json())
+      .then(dataFromApi => {
+        this.setState({ data: dataFromApi });
+      });
+  };
+
+  render() {
+    const { goToListing, detailId } = this.props;
+    console.log('detailId', detailId);
+    const { data } = this.state;
+    console.log('Data from state are', data);
+
+    return (
+      <div>
+        <h2>This is detail</h2>
+        <button onClick={goToListing}>Go to Listing</button>
+      </div>
+    );
+  }
 }
 
 export default Detail;
