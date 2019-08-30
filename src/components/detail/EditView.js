a73f4a3d2074be571b95076ddb4c628c98b4e732
JanSramek2018 <jsramek@hotmail.cz>
2019-08-30 18:37:52 +0200

    Update ingredienci, zbyva nacist do API

--- src/components/detail/EditView.js
+++ src/components/detail/EditView.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import IngredientsEdit from './IngredientsEdit';
+import { isTemplateElement } from '@babel/types';
 
 const API_URL = 'https://cookbook.jakubricar.cz/api/recipes/';
 
@@ -12,13 +14,24 @@ class EditView extends React.Component {
   }
 
   handleChange = event => {
+
+    const { name, value } = event.target;
+
     this.setState({
       dataToUpdate: {
         ...this.state.dataToUpdate,
-        title: event.target.value,
-        directions: event.target.value,
+        [name]: value,
       },
     });
+  };
+
+  handleDelete = itemId => {
+    console.log('handling delete in EDITVIEW id: ' + itemId);
+    const newIngredients = this.state.dataToUpdate.ingredients.filter(item => item._id !== itemId);
+    console.log(newIngredients);
+    this.setState({...this.state.dataToUpdate, ingredients: newIngredients});
+    const ingredients = this.state.dataToUpdate.ingredients
+    console.log(ingredients);
   };
 
   submitData = () => {
@@ -41,19 +54,41 @@ class EditView extends React.Component {
   render() {
     const { dataToUpdate } = this.state;
     const { title } = dataToUpdate;
+    const { preparationTime } = dataToUpdate;
+    const { servingCount } = dataToUpdate;
+    const { sideDish } = dataToUpdate;
+    const { ingredients } = dataToUpdate;
     const { directions } = dataToUpdate;
+    const { _id } = dataToUpdate;
 
     return (
       <div>
         <h3>EDIT</h3>
         <form action="#" onSubmit={this.submitData}>
           <div>
-            <label>Edit title </label>
-            <input type="text" value={title} onChange={this.handleChange} />
+            <h2>Edit Basic info</h2>
+            <label>ID </label>
+            <input type="text" name="_id" value={_id} onChange={this.handleChange} />
+            <label>Title </label>
+            <input type="text" name="title" value={title} onChange={this.handleChange} />
           </div>
           <div>
-            <label>Edit Description </label>
-            <input type="text" value={directions} onChange={this.handleChange} />
+            <label>Preparation time </label>
+            <input type="text" name="preparationTime" value={preparationTime} onChange={this.handleChange} />
+          </div>
+          <div>
+            <label>Number of portions </label>
+            <input type="text" name="servingCount" value={servingCount} onChange={this.handleChange} />
+          </div>
+          <div>
+            <label>Side dish </label>
+            <input type="text" name="sideDish" value={sideDish} onChange={this.handleChange} />
+          </div>
+          <div>
+            <h2>Edit Ingredients</h2>
+            <IngredientsEdit ingredients={ingredients} onDelete={this.handleDelete} />
+            <h2>Edit Description</h2>
+            <input type="text" name="directions" value={directions} onChange={this.handleChange} />
           </div>
           <input type="submit" value="Submit" />
         </form>
---
+++ src/components/detail/IngredientsEdit.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class IngredientsEdit extends React.Component {
+
+
+  render() {
+    const { ingredients } = this.props;
+
+    const generateIngredientRow = (ingredient, index) => {
+      const { _id, name, amount, amountUnit } = ingredient;
+      return (
+        
+        <tr key={index} value={_id}>
+          <td>{amount}</td>
+          <td>{amountUnit}</td>
+          <td>{name}</td>
+          <td>{_id}</td>
+          <td><button onClick={() => this.props.onDelete(_id)}>Delete-notworking yet</button></td>
+        </tr>
+      );
+    };
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Amount</th>
+            <th>Unit</th>
+            <th>Name</th>
+            <th>Id-Delete later</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>{ingredients.map(generateIngredientRow)}</tbody>
+      </table>
+    );
+  }
+}
+
+export default IngredientsEdit;
